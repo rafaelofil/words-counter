e13fcfe0a8673aae882e12ab46021f287ab736ba
fortpol <rafaelofil@outlook.com>
2023-05-07 23:32:22 -0300

    word counter]

---
+++ src/words_counter.rb
@@ -0,0 +1,22 @@
+TEXT_FILE = "../rome-and-juliet.txt"
+REFERENCE_TEXT_FILE = "../hamlet.txt"
+
+def words_from_file(text_file)
+  File.read(text_file).downcase.gsub(/[^a-z]/, " ").split
+  rescue
+    puts "Give me #{text_file} and let's get the party started!"
+    exit
+end
+
+words = words_from_file(TEXT_FILE)
+words_to_remove = words_from_file(REFERENCE_TEXT_FILE).uniq
+
+words_to_remove.each do |word|
+  words.delete word
+end
+
+WORD_COUNT = {}
+words.each do |word|
+  WORD_COUNT[word] = 0 unless WORD_COUNT[word]
+  WORD_COUNT[word] += 1
+end
